2cd30b0e65b430313eeb4c9409d35cc2cd0965d4
harry <sanmiguella@hotmail.com>
2019-08-21 23:12:12 +0800

    Create constants.php

---
+++ PHP/constants.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <title>Document</title>
+    <link rel='stylesheet' href='mycss.css' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+
+<?php
+
+define("NAME", "harry"); // Defining a constant. 
+echo "Hi " . NAME . "!<br>"; // Display the value of a constant using echo.
+
+?>
+
+</body>
+</html>
