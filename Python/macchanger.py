c745c566f03dc2430cc705b5026744ac56be8512
evdaez <50333912+sanmiguella@users.noreply.github.com>
2019-11-16 22:08:01 -0500

    Add files via upload

--- Python/macchanger.py
+++ Python/macchanger.py
@@ -1,25 +1,31 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+
+# Import the required modules for this program to work
 import subprocess
 import optparse
 
-parser = optparse.OptionParser() # To simplify accessing method()
+# To restore to original mac addr:
+# root@kali:~/scripts# ethtool -P eth0
 
-# Command switch as well as displaying help in case user types -> python maccchanger.py -h
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="newMac", help="New mac address")
+# Function must be defined first here before calling it later
+def changeMacAddress(interface, new_mac):
+    print('[+] Changing mac address for ' + interface + ' to ' + new_mac + '\n') 
+
+    subprocess.call(['ifconfig', interface, 'down']) # Brings interface down
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac]) # Changes MAC addr
+    subprocess.call(['ifconfig', interface, 'up']) # Brings interface up
+    subprocess.call(['ifconfig', interface]) # Shows the interface details
+
+
+parser = optparse.OptionParser() # Simplifies acessing methods so we are able to use shorthand instead of typing everything in full
+
+# Arguments for the program:
+# -i : interface , -m : mac address , -h : help
+parser.add_option("-i", "--interface", dest="interface", help="Interface for mac address change")
+parser.add_option("-m", "--mac", dest="new_mac", help="New mac address") 
 
 (options, arguments) = parser.parse_args()
 
-# Value of -i, -m are contained inside options properties(values/variables)
-# Methods = functions()
-# Properties = variables
-interface = options.interface
-newMac = options.newMac
+# Calls the changeMacAddress() function
+changeMacAddress(options.interface, options.new_mac)
 
-print("[+] Changing mac address for interface " + interface + " to " + newMac + "\n")
-
-# Every command is a separate string
-subprocess.call(["ifconfig", interface, "down"]) # Brings interface down 
-subprocess.call(["ifconfig", interface, "hw", "ether", newMac]) # Change interface mac address to new address
-subprocess.call(["ifconfig", interface, "up"]) # Brings interface up
-subprocess.call(["ifconfig", interface]) # Shows user the interface values
