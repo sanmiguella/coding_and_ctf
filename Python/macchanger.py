5faa753620fcf903bcd899c399d0544309c0daed
evdaez <50333912+sanmiguella@users.noreply.github.com>
2019-11-18 16:17:18 +0800

    Add files via upload

--- Python/macchanger.py
+++ Python/macchanger.py
@@ -7,6 +7,18 @@ import optparse
 # To restore to original mac addr:
 # root@kali:~/scripts# ethtool -P eth0
 
+def getArguments():
+    parser = optparse.OptionParser() # Simplifies acessing methods so we are able to use shorthand instead of typing everything in full
+
+    # Arguments for the program:
+    # -i : interface , -m : mac address , -h : help
+    parser.add_option("-i", "--interface", dest="interface", help="Interface for mac address change")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New mac address") 
+
+    # Return values to the calling function()
+    return parser.parse_args()
+
+
 # Function must be defined first here before calling it later
 def changeMacAddress(interface, new_mac):
     print('[+] Changing mac address for ' + interface + ' to ' + new_mac + '\n') 
@@ -17,15 +29,5 @@ def changeMacAddress(interface, new_mac):
     subprocess.call(['ifconfig', interface]) # Shows the interface details
 
 
-parser = optparse.OptionParser() # Simplifies acessing methods so we are able to use shorthand instead of typing everything in full
-
-# Arguments for the program:
-# -i : interface , -m : mac address , -h : help
-parser.add_option("-i", "--interface", dest="interface", help="Interface for mac address change")
-parser.add_option("-m", "--mac", dest="new_mac", help="New mac address") 
-
-(options, arguments) = parser.parse_args()
-
-# Calls the changeMacAddress() function
+(options, arguments) = getArguments()
 changeMacAddress(options.interface, options.new_mac)
-
