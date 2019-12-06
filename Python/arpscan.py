93221a12dd2008777c4b9b6c31f9c775fc243ead
evdaez <50333912+sanmiguella@users.noreply.github.com>
2019-12-06 14:42:58 +0800

    Add files via upload

--- Python/arpscan.py
+++ Python/arpscan.py
@@ -3,14 +3,60 @@
 # So we won't need to type scapy.all
 import scapy.all as scapy
 
-def scan(ip):
-	# pdst :IPField -> Destination
-	# ARP who has Net('192.168.218.1/24') says 192.168.218.163
-	arp_request = scapy.ARP(pdst=ip)
+def scan(ip):	
+	'''
+	###[ ARP ]### 
+	  hwtype    = 0x1
+	  ptype     = 0x800
+	  hwlen     = 6
+	  plen      = 4
+	  op        = who-has
+	  hwsrc     = 00:0c:29:ef:10:a7
+	  psrc      = 192.168.40.149
+	  hwdst     = 00:00:00:00:00:00
+	  pdst      = Net('192.168.40.1/24')
+	'''
+	arp_request = scapy.ARP(pdst=ip) 
+	arp_request.show()
+	separate()
 
-	print(arp_request.summary()) + "\n"
+	'''
+	###[ Ethernet ]### 
+	  dst       = ff:ff:ff:ff:ff:ff
+	  src       = 00:0c:29:ef:10:a7
+	  type      = 0x9000
+	'''
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") 
+	broadcast.show()
+	separate()
+	
+	'''
+	###[ Ethernet ]### 
+	  dst       = ff:ff:ff:ff:ff:ff
+	  src       = 00:0c:29:ef:10:a7
+	  type      = 0x806
+	###[ ARP ]### 
+	     hwtype    = 0x1
+	     ptype     = 0x800
+	     hwlen     = 6
+	     plen      = 4
+	     op        = who-has
+	     hwsrc     = 00:0c:29:ef:10:a7
+	     psrc      = 192.168.40.149
+	     hwdst     = 00:00:00:00:00:00
+	     pdst      = Net('192.168.40.1/24')
+	'''
+	arp_request_broadcast = broadcast/arp_request
+	arp_request_broadcast.show()
+
+	# print(arp_request.summary())
+	# print(broadcast.summary())
 
 	# Listing of various fields which could be customized
-	#scapy.ls(scapy.ARP())
-	
-scan("192.168.218.1/24")
+	# scapy.ls(scapy.ARP())
+	# scapy.ls(scapy.Ether())	
+
+def separate():
+	print "=" * 50 + "\n"
+
+scan("192.168.40.1/24")
