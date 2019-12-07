202eebdb2830aba1315293b047a4d1397c98cd09
evdaez <50333912+sanmiguella@users.noreply.github.com>
2019-12-07 10:30:33 +0800

    Add files via upload

--- Python/network_scanner.py
+++ Python/network_scanner.py
@@ -3,19 +3,27 @@
 import scapy.all as scapy
 
 def scan(ip):
+	# List down ARP fields
 	scapy.ls(scapy.ARP())
 	print "\n"	
 	
+	# List down Ethernet fields
 	scapy.ls(scapy.Ether())
 	print "\n"
 
+	# Set the required destination IP field
 	arp_request = scapy.ARP(pdst=ip)
 	arp_request.show()
 
+	# Set the required destination MAC field
 	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 	broadcast.show()
 
+	# To broadcast to the whole subnetwork: 192.168.218.1/24
+	# Destination broadcast MAC address ff:ff:ff:ff:ff:ff
 	arp_request_broadcast = broadcast / arp_request
+
+	# srp - send and receive
 	answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)	
 
 	print str(answered.summary()) + "\n"
