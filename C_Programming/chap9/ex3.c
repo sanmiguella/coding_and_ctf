2b1f6731bfea923b46af6bcdeeb841de1834f2f1
harry <sanmiguella@hotmail.com>
2020-03-18 15:40:38 +0800

    done

--- C_Programming/chap9/ex3.c
+++ C_Programming/chap9/ex3.c
@@ -3,13 +3,16 @@
 
 int main() 
 {
-    char greeting[] = "Hello\n";    // Declares an array of characters
+    char greeting[] = "Hello\n";           // Declares an array of characters
+    int count;
+    int max_count = strlen(greeting);
 
     printf("Press enter...."); 
-    getchar();                      // Get any character input and program will continue to the next instruction
+    getchar();                            // Get any character input and program will continue to the next instruction
 
-    for(int i = 0; i < strlen(greeting); i++) {
-        putchar(greeting[i]);       // Loops through every char in the array and print every char out to the console
+    for(count = 0; count < max_count; count++) {
+        char msg = greeting[count];
+        putchar(msg);                     // Loops through every char in the array and print every char out to the console
     }
 
     return(0);
