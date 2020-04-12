b80b250fa16ba2036a135edceca185438a7ccaf4
harry <sanmiguella@hotmail.com>
2020-04-12 20:21:48 +0800

    Update ex4.py

--- PSEC/Practical 3/ex4.py
+++ PSEC/Practical 3/ex4.py
@@ -1,23 +1,32 @@
 import os
 import time
 
+# Constant for time.sleep()
+SLEEP_DURATION = 1.2
+
+# List in global scope which is accessible to all functions
 marks = []
 
 def clear():
+    # To clear screen, provides a more better visual experience
     os.system("cls")
 
 def pause():
+    # No further action unless any other key is pressed
     os.system("pause")
 
 def mini_pause():
-    time.sleep(1)
+    # Pause for a short time
+    time.sleep(SLEEP_DURATION)
 
 def display_marks():
+    # If marks list isn't empty, display all the elements in the list
     if (len(marks) > 0):
             print("Current marks for %d students stored in system." % len(marks))
             print(marks)
 
 def max_marks():
+    # If marks list isn't empty, determine the highest value in the list and output the results to the console
     if len(marks) > 0:
         print("\nHighest Mark: %d" % max(marks))
         pause()
@@ -27,9 +36,11 @@ def max_marks():
         mini_pause()
 
 def sort_marks():
+    # If marks list isn't empty, show elements of the list in ascending order
     if len(marks) > 0:
         ascending_marks = marks.copy()
         ascending_marks.sort()
+
         print("\nMarks in ascending order: %s" % ascending_marks)
         pause()
     
@@ -38,34 +49,40 @@ def sort_marks():
         mini_pause()
 
 def splice():
+    # If there are no global keyword, trying to modify marks will make it seem to the program that the scope is local and as such it will raise an error as there are no local marks list defined
     global marks
 
+    # Will attempt to do the splicing only if the marks list isn't empty
     if len(marks) > 0:
         try:
             clear()
             
-            tmp_list = []
+            tmp_list = [] # To temporarily store the sliced list
             
             display_marks()
 
             start_index = int(input("\nInput start of index to subset: "))
             end_index = int(input("Input end of index to subset: "))
 
+            # Error checking conditions that prevents the index from going out of bounds
             if start_index < 0 or start_index > len(marks) or end_index < 0 or end_index > len(marks):
                 print(
                     "\nStart/End index must not be lesser than 0 or greater than the length of the list of marks!")
                 pause()
 
+            # If start index > end index, there will be no data displayed 
             elif start_index > end_index:
                 print("\nStart index must not be greater than end index!")
                 pause()
 
+            # If start index = end index, there will be no data displayed
             elif start_index == end_index:
                 print("\nStart/End index must not be equal to each other!")
                 pause()
 
+            # If prior conditions has been satisfied, proceed to slice the list, data will be stored in a temporary list and will be copied back to the old list
             else:
-                tmp_list = marks[start_index: end_index]
+                tmp_list = marks[start_index : end_index]
                 marks = tmp_list.copy()
             
                 print("\n%s" % marks)
@@ -80,6 +97,7 @@ def splice():
         mini_pause()
 
 def remove_mark():
+    # If marks list isn't empty, proceed to prompt user to enter index of elements to be removed
     if len(marks) > 0:
         clear()
         display_marks()
@@ -87,14 +105,17 @@ def remove_mark():
         try:
             remove_index = int(input("\nIndex to remove: "))
             
+            # Error checking to prevent index from going out of bound
             if remove_index < 0 or remove_index >= len(marks):
                 print("\nIndex must be between 0 to (length of marks -1)")
                 pause()
             
+            # Removes the element of a particular index and outputs the result to the console
             else:
                 del(marks[remove_index])
                 clear()
                 display_marks()
+                print("")
                 pause()
 
         except ValueError:
@@ -106,8 +127,7 @@ def remove_mark():
         mini_pause()
 
 def change_mark():
-    global marks
-
+    # If marks list isn't empty, proceed to prompt user to enter the index of the element whose value needs to be changed
     if len(marks) > 0:
         clear()
         display_marks()
@@ -115,6 +135,7 @@ def change_mark():
         try:
             change_index = int(input("\nIndex to change: "))
 
+            # Error checking to prevent out of bounds issue
             if change_index < 0 or change_index >= len(marks):
                 print("\nIndex must be between 0 to (length of marks -1)")
                 pause()
@@ -123,10 +144,12 @@ def change_mark():
                 try:
                     new_value = int(input("Input new value: "))
 
+                    # Error checking that only accepts values of 0-100
                     if new_value >=0 and new_value <= 100:
                         marks[change_index] = new_value
                         clear()
                         display_marks()
+                        print("")
                         pause()
                     
                     else:
@@ -145,19 +168,19 @@ def change_mark():
         print("\nMarks list is empty!")
         mini_pause()
 
-def add_mark():
-    global marks
-    
+def add_mark(): 
     clear()
     display_marks()
 
     try:
         new_mark = int(input("\nInput new mark: "))
 
+        # Error checking that only allows accepts value of 0-100
         if new_mark >=0 and new_mark <= 100:
             marks.append(new_mark)
             clear()
             display_marks()
+            print("")
             pause()
         
         else:
@@ -177,9 +200,10 @@ def edit_list_menu():
     print("5. Back to main menu")
 
     choice = input("\nPlease choose on how you want to edit the list: ")
-    return(choice)
+    return(choice) 
 
 def edit_list():
+    # Only allows marks list to be edited as long as it is not an empty list
     if len(marks) > 0:
         while True:
             clear()
@@ -211,29 +235,28 @@ def edit_list():
 def entry():
     clear()
 
+    # Loops until a -1 is entered
     while True:
         try:
             clear()
-            count = len(marks) + 1
+            count = len(marks) + 1 # For subsequent entries
 
             display_marks()
 
             mark_input = int(input("\nPlease enter marks for student %d or -1 to end: " % count))
 
+            # If input entered is -1, exit while loop
             if mark_input == -1:
                 break
 
-            elif mark_input < 0:
-                print("\nNegative values other than -1 is not valid.")
+            # Error checking that accepts only values of 0-100
+            elif mark_input < 0 or mark_input > 100:
+                print("\nMarks entered must be in the range of 0-100!")
                 mini_pause()
 
-            elif mark_input > 100:
-                print("\nValid marks are between 0 to 100.")
-                mini_pause()
-
+            # If prior conditions are met, proceed to add new elements to the mark list
             else:
                 marks.append(mark_input)
-                count += 1
 
         except ValueError:
             print("\nThis is not a valid mark")
