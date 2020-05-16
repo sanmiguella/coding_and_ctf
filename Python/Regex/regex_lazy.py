580bc7f74be3cb38794e602645879e25ab2f07df
harry <sanmiguella@hotmail.com>
2020-05-17 07:50:14 +0800

    Update regex_lazy.py

--- Python/Regex/regex_lazy.py
+++ Python/Regex/regex_lazy.py
@@ -5,7 +5,7 @@ sentiment_analysis = "Put vacation photos online(They were so cute) a few yrs ag
 #  \d digit,\w word character, \W special char, \s whitespace
 # * zero or more times, + once or more, ? zero or once.
 
-brackets_greedy_regex = r"\(.+?\)"
-brackets_lazy_regex = r"\(.*?\)"
+brackets_lazy_regex = r"\(.+?\)"
+brackets_greedy_regex = r"\(.*?\)"
 
 print(re.findall(brackets_lazy_regex, sentiment_analysis))
