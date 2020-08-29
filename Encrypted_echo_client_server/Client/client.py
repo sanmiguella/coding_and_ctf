08089ba23e3a5014d1bcfb1ebc937717e6cdcad8
maskcrafter <escuse_me_mr@hotmail.com>
2020-08-29 23:22:03 +0800

    testing

---
+++ Encrypted_echo_client_server/Client/client.py
@@ -0,0 +1,80 @@
+import socket
+
+from os import system
+from Security import Security
+from base64 import b64encode, b64decode
+ 
+class Client(Security):
+    def __init__(self):
+        super().__init__()
+        self.target = "127.0.0.1"
+        self.port = 4444
+        self.def_encoding = "utf-8"
+        self.buffer_size = 128
+
+    def clear_screen(self):
+        system("cls")
+
+    def pause(self):
+        system("pause")
+
+    def client_sender(self):
+        try:
+            while True:
+                self.clear_screen()
+            
+                data_list = list() 
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                
+                buffer = input("[#] Input - ")
+                encrypted_data = self.generate_encrypted_data(buffer)
+                print(f"\n[!] Encrypted data ({len(encrypted_data)} Bytes) - {encrypted_data}")
+                
+                # 344 bytes - RSA encrypted session key, After 344 bytes - iv & ciphertext
+                client_socket.connect((self.target, self.port))
+                client_socket.sendall(encrypted_data.encode(self.def_encoding))                
+                client_socket.shutdown(socket.SHUT_WR)
+
+                count = 1
+                print(f"\n[*] Send & Receive buffer - ({self.buffer_size} Bytes)")
+    
+                while True:
+                    data_block = client_socket.recv(self.buffer_size).decode(self.def_encoding)
+                    print(f"[~] Data Block {count} ({len(data_block)} Bytes)- {data_block}")
+
+                    if not data_block:
+                        break
+
+                    data_list.append(data_block)
+                    count += 1
+
+                received_data = ""
+
+                for data in data_list:
+                    received_data += data
+
+                print(f"\n[=] Data list - {data_list}")
+                print(f"[=] Received data ({len(received_data)} Bytes) - {received_data}")
+
+                decrypted_data = self.decrypt_received_data(received_data)
+                print(f"\n[>] Decrypted data ({len(decrypted_data)} Bytes) - {decrypted_data}\n")
+
+                self.pause()
+
+        except KeyboardInterrupt:
+            print(f"\n\n[!] Ctrl+C detected, exiting client.")
+
+        except socket.error as socket_error:
+            print(f"\n[!] Socket Error - {socket_error}")
+
+        except Exception as error:
+            print(f"\n[!] Error - {error}")
+
+        finally:
+            client_socket.close()
+
+    def start(self):
+        self.client_sender()
+               
+client = Client()
+client.start()
---
+++ Encrypted_echo_client_server/Server/server.py
@@ -0,0 +1,95 @@
+import socket
+import threading
+import sys, signal
+
+from os import getcwd, system
+from Security import Security
+from base64 import b64encode, b64decode
+
+class Server(Security):
+    def __init__(self):
+        super().__init__()
+        self.bind_ip = "127.0.0.1"
+        self.bind_port = 4444
+        self.log_file = getcwd() + "\\Logs\\server_log.txt"
+        self.buffer_size = 128
+        self.def_encoding = "utf-8"
+
+    def print_and_log(self, data):
+        with open(self.log_file, "a+") as log_file:
+            log_file.write(f"{data}\n")
+            print(data)
+    
+    def clear_screen(self):
+        system("cls")
+
+    def handle_client(self, client_socket):
+        data_list = list()
+        count = 1
+        received_data = ""
+
+        try:
+            print(f"\n[*] Send & Recv buffer - ({self.buffer_size} Bytes)")
+
+            while True:
+                data_block = client_socket.recv(self.buffer_size).decode(self.def_encoding)
+                self.print_and_log(f"[~] Data Block {count} ({len(data_block)} Bytes) - {data_block}")
+
+                if not data_block:
+                    break
+
+                client_socket.sendall(received_data.encode(self.def_encoding))
+                data_list.append(data_block)
+                count += 1
+
+            for data in data_list:
+                received_data += data
+
+            self.print_and_log(f"\n[=] Data list - {data_list}")
+            self.print_and_log(f"[=] Received data ({len(received_data)} Bytes)- {received_data}")
+
+            decrypted_data = self.decrypt_received_data(received_data)
+            self.print_and_log(f"\n[>] Decrypted data ({len(decrypted_data)} Bytes) - {decrypted_data}")
+
+            encrypted_reply = self.generate_encrypted_data(decrypted_data)
+
+            client_socket.sendall(encrypted_reply.encode(self.def_encoding))
+            self.print_and_log(f"\n[+] Sending back reply ({len(encrypted_reply)} Bytes) - {encrypted_reply}")
+
+            client_socket.close()
+            self.print_and_log(f"[#] Closed client connection.")
+
+        except ConnectionResetError as error:
+            self.print_and_log(f"\n[!] Connection Reset Error - {error}")
+
+    def start(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        self.clear_screen()
+        self.print_and_log(f"[#] Server starting.")
+        server.bind((self.bind_ip, self.bind_port))
+
+        server.listen()
+        self.print_and_log(f"[#] Listening on ({self.bind_ip} : {self.bind_port})")
+        
+        while True:
+            try:
+                client, addr = server.accept()
+                client_ip, client_port = addr
+
+                self.print_and_log(f"\n[+] Connection accepted - ({client_ip} : {client_port})")
+                client_handler = threading.Thread(target=self.handle_client, args=(client,))
+                client_handler.start()
+            
+            except KeyboardInterrupt:
+                self.print_and_log(f"\n[!] CTRL+C pressed, goodbye!")
+                break
+
+            except socket.error as socket_error:
+                self.print_and_log(f"\n[!] Socket Error - {socket_error}")
+        
+            except Exception as error:
+                self.print_and_log(f"\n[!] Error - {error}")
+
+server = Server()
+server.start()
