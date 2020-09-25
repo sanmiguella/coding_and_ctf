46dacf8dec9df08105769870a6185c89a0b89ac8
maskcrafter <escuse_me_mr@hotmail.com>
2020-09-25 22:47:42 +0800

    update

--- Encryptor/encryptor.py
+++ Encryptor/encryptor.py
@@ -24,50 +24,75 @@ class Encryption:
 
         key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
         return key
+    
+    @classmethod
+    # For use in decrypting filename.
+    # Plaintext - must be in binary format.
+    # get_key() will automatically encode password.
+    def decrypt(cls, password, cipherText):
+        key = cls.get_key(password)
+        crypto = Fernet(key)
+
+        plainText = crypto.decrypt(cipherText)
+        return plainText
 
     @classmethod
-    def encrypt_file(cls, password, src_filename, dest_filename):
+    # For use in encrypting filename.
+    # Plaintext - must be in binary format.
+    # get_key() will automatically encode password.
+    def encrypt(cls, password, plainText):
         key = cls.get_key(password)
-
         crypto = Fernet(key)
-        index = 0
+
+        cipherText = crypto.encrypt(plainText)
+        return cipherText
+
+    @classmethod
+    def encrypt_file(cls, password, src_filename):
+        key = cls.get_key(password)
+        crypto = Fernet(key)
+        
+        # destination filename must be in string format.
+        dest_filename = Encryption.encrypt(password, src_filename.encode()).decode()
 
         try:
             with open(src_filename, "rb") as src_file:
                 with open(dest_filename, "w") as dest_file:
                     plainText_block = src_file.read(cls.DATA_BLOCK_SIZE)
 
+                    index = 0
                     while plainText_block != b"":
                         cipherText_block = crypto.encrypt(plainText_block)
                         
                         # To remove newlines at the end of the file.
                         if index == 0:
                             dest_file.write(f"{cipherText_block.decode()}")
+                            index += 1
+
                         else:
                             dest_file.write(f"\n{cipherText_block.decode()}")
                         
                         plainText_block = src_file.read(cls.DATA_BLOCK_SIZE)
-                        index += 1
     
         except AssertionError:
             sys.exit(1)
 
     @classmethod
-    def decrypt_file(cls, password, src_filename, dest_filename):
+    def decrypt_file(cls, password, src_filename):
         key = cls.get_key(password)
-
         crypto = Fernet(key)
-        index = 0
 
+        # destination filename must be in string format.
+        dest_filename = Encryption.decrypt(password, src_filename.encode()).decode()
+
+        # Split using newlines as delimeter.
         cipherText_list = open(src_filename).read().splitlines()
-
+        
         try:
             with open(dest_filename, "w", newline = "\n") as df:
                 for cipherText in cipherText_list:
                     plainText = crypto.decrypt(cipherText.encode()).decode()
-
                     df.write(plainText)
-                    index += 1
 
         except AssertionError:
             sys.exit(1)
@@ -75,18 +100,24 @@ class Encryption:
 class Program:
     @classmethod
     def show_help(cls):
-        # Only get program name, not interested in full path.
-        programName = sys.argv[0].split('\\')
-        programName = programName[len(programName) -1]
-
+        programName = Program.get_name()
         headerName = "Program Usage"
 
         print("-" * 64)
         print(f"{headerName:>35}")
         print("-" * 64)
-        print(" Command - encrypt or decrypt\n")
-        print(f" {programName} command plainTextFile.txt cipherTextFile.txt")
-        print(f" {programName} command cipherTextFile.txt plainTextFile.txt")
+
+        print()
+        print(f" {programName} encrypt plainTextFile.txt")
+        print(f" {programName} decrypt cipherTextFile.txt")
+
+    @classmethod
+    def get_name(cls):
+        # Only get program name, not interested in full path.
+        programName = sys.argv[0].split('\\')
+        programName = programName[len(programName) -1]
+
+        return programName
 
     @classmethod
     def show_args(cls):
@@ -100,7 +131,7 @@ class Program:
         # sys.argv[0] - program name
         # sys.argv[1] - commands or actions
 
-        if len(sys.argv) < 5:
+        if len(sys.argv) < 4:
             Program.show_help()
         
         else:
@@ -112,15 +143,14 @@ class Program:
             else:
                 password = sys.argv[2].strip()
                 src_file = sys.argv[3].strip() 
-                dest_file = sys.argv[4].strip()
 
                 if path.isfile(src_file):
                     if command == "encrypt":
-                        Encryption.encrypt_file(password, src_file, dest_file)
+                        Encryption.encrypt_file(password, src_file)
                         print("\nEncryption Done!")
 
                     elif command == "decrypt":
-                        Encryption.decrypt_file(password, src_file, dest_file)
+                        Encryption.decrypt_file(password, src_file)
                         print("\nDecryption Done!")
 
                 else:
