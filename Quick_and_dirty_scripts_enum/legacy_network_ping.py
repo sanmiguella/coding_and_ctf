5577e417b2bca54420ff9d0f644fe70cd09164d8
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-03-10 00:10:32 +0800

    Create legacy_network_ping.py

---
+++ Quick_and_dirty_scripts_enum/legacy_network_ping.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import ipaddress
+import argparse
+from ping3 import ping
+
+responsive_hosts = []
+
+def save_hosts_to_file(save_file):
+    with open(save_file,'w') as sf:
+        for host in responsive_hosts:
+            sf.write(f"{host}\n")
+
+if __name__ == "__main__":
+    ipv4_subnet = []
+
+    parser = argparse.ArgumentParser(description="Ping legacy ipv4 network")
+    parser.add_argument("ipv4_subnet", help="Enter CIDR, ex 192.168.2.0/24")
+
+    args = parser.parse_args()
+    ipv4_subnet = args.ipv4_subnet
+    ipv4_hosts = [str(ip) for ip in ipaddress.IPv4Network(ipv4_subnet)]
+
+    for host in ipv4_hosts:
+        try:
+            host = host.strip()
+
+            print(f"\n--> Pinging {host} <--\n")
+            response = ping(host, unit='ms')
+
+            if response == False:
+                print("[!] Host unknown.")
+            elif response == None:
+                print("[!] Timed out.")
+            else:
+                print(f"Response from {host} :: {response} ms")
+                responsive_hosts.append(host)
+
+        except Exception as err:
+            pass
+
+    print("\n[+] List of responsive hosts:\n")
+    for host in responsive_hosts:
+        print(host)
+
+    save_file = "./ipv4_hosts.txt"
+    print(f"\n[+] Saving to {save_file}\n")
+    save_hosts_to_file("./ipv4_hosts.txt")
+
