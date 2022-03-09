a2fdead2543c42cd8e727c8cb2d452378cdd8798
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-03-09 20:39:58 +0800

    test

--- Quick_and_dirty_scripts_enum/getIPv6.py
+++ Quick_and_dirty_scripts_enum/getIPv6.py
@@ -2,7 +2,7 @@
 import argparse
 import socket
 
-ipv4_list = []
+ipv6_list = []
 
 def read_from_file(file_to_read):
     with open(file_to_read,'r') as f:
@@ -10,16 +10,16 @@ def read_from_file(file_to_read):
 
     return(hostnames)
 
-def save_ipv4_list(filename):
-    # Sort and save ipv4s in the list to a file
-    ipv4_list.sort()
+def save_ipv6_list(filename):
+    # Sort and save ipv6s in the list to a file
+    ipv6_list.sort()
 
-    with open(filename,'w') as ipv4_file:
-        for ipv4 in ipv4_list:
-            ipv4_file.write(f"{ipv4}\n")
+    with open(filename,'w') as ipv6_file:
+        for ipv6 in ipv6_list:
+            ipv6_file.write(f"{ipv6}\n")
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(description="Get IPv4 from hostname")
+    parser = argparse.ArgumentParser(description="Get IPv6 from hostname")
     parser.add_argument("file_to_read", help="Enter file containing a list of hostnames")
 
     args = parser.parse_args()
@@ -31,11 +31,12 @@ if __name__=="__main__":
 
         try:
             print(f"\n[{index}] ---> Resolving \"{hostname}\" <---\n")
-            ipv4 = socket.gethostbyname(hostname)
-            ipv4_list.append(f"{hostname} {ipv4}")
-            print(f"IPv4 : {ipv4}")
+            result = socket.getaddrinfo(hostname, 443, socket.AF_INET6)
+            ipv6 = result[0][4][0]
+            ipv6_list.append(f"{hostname} {ipv6}")
+            print(f"IPv6 : {ipv6}")
 
         except Exception as err:
             print(f"Error -> {err}")
 
-    save_ipv4_list("./ipv4_list.txt")
+    save_ipv6_list("./ipv6_list.txt")
