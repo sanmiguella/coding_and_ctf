2397cca0bc6380fa6f6d9d06df75c29dbbce8452
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-03-12 16:46:59 +0800

    added threading

--- Quick_and_dirty_scripts_enum/scan.py
+++ Quick_and_dirty_scripts_enum/scan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import requests 
+import requests
 import argparse
+import concurrent.futures
 
 valid = []
 maybe_valid = []
@@ -8,40 +9,54 @@ maybe_valid = []
 def read_from_file(file_to_read):
     with open(file_to_read,'r') as f:
         urls = f.readlines()
+
     return(urls)
 
 def save_valid_hosts():
-    # Save valid and maybe_valid to file
-    with open("./valid.txt",'w') as valid_file:
+    vf = "./valid.txt"
+
+    with open(vf, 'w') as valid_file:
         for valid_url in valid:
             valid_file.write(valid_url + "\n")
 
+    print(f"[+] Written valid hosts to :: {vf}")
+
 def save_maybe_valid_hosts():
-    with open("./maybe_valid.txt",'w') as maybe_valid_file:
+    mvf = "./maybe_valid.txt"
+
+    with open(mvf, 'w') as maybe_valid_file:
         for maybe_valid_url in maybe_valid:
             maybe_valid_file.write(maybe_valid_url + "\n")
+
+    print(f"[+] Written maybe valid hosts to :: {mvf}")
+
+def initiate_request(url):
+    try:
+        response = requests.get(url)
+        response_code = response.status_code
+        print(f"{url} :: {response_code}")
+
+        if response_code == 200:
+            valid.append(url)
+        else:
+            maybe_valid.append(url)
+
+    except Exception as err:
+        print(f"{url} :: {err}")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Check host(s) for code 200.')
     parser.add_argument("file_to_read", help="Enter file containing a list of hosts")
 
     args = parser.parse_args()
-    urls = read_from_file(args.file_to_read.strip())
+    urls = read_from_file(args.file_to_read)
+    urls_stripped = [url.strip() for url in urls]
 
-    # Check URL 1 by 1
-    for index, url in enumerate(urls):
-        url = f"https://{url.strip()}"
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        for url in urls_stripped:
+            url = f"https://{url}"
+            executor.submit(initiate_request, url)
 
-        try:
-            print(f"{index} ---> Trying {url} <---")
-            res = requests.get(url)
-            res_code = res.status_code
-            print(f"\nResponse code: {res_code}\n")
-
-            if res_code == 200:
-                valid.append(url)
-            else:
-                maybe_valid.append(url)
-
-        except Exception as err:
-            print(f"\nErr -> {err}\n")
+    print()
+    save_valid_hosts()
+    save_maybe_valid_hosts()
