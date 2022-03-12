76fcbf0dda52908f132a4b88eb09882c591e7f04
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-03-12 15:50:20 +0800

    added threading

--- Quick_and_dirty_scripts_enum/getIPv4.py
+++ Quick_and_dirty_scripts_enum/getIPv4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import socket
+import concurrent.futures
 
 ipv4_list = []
 
@@ -10,6 +11,16 @@ def read_from_file(file_to_read):
 
     return(hostnames)
 
+def resolve_hostname(hostname):
+    try:
+        ipv4 = socket.gethostbyname(hostname)
+        ipv4_list.append(f"{hostname} {ipv4}")
+        print(f"{hostname} :: {ipv4}")
+
+    except Exception as err:
+        #print(f"{hostname} :: {err}")
+        pass
+
 def save_ipv4_list(filename):
     # Sort and save ipv4s in the list to a file
     ipv4_list.sort()
@@ -18,24 +29,18 @@ def save_ipv4_list(filename):
         for ipv4 in ipv4_list:
             ipv4_file.write(f"{ipv4}\n")
 
+    print(f"\n[+] Saved file to {filename}")
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Get IPv4 from hostname")
     parser.add_argument("file_to_read", help="Enter file containing a list of hostnames")
 
     args = parser.parse_args()
-    hostnames = read_from_file(args.file_to_read.strip())
+    hostnames = read_from_file(args.file_to_read)
+    stripped_hostnames = [hostname.strip() for hostname in hostnames]
 
-    # Check hostname 1 by 1
-    for index, hostname in enumerate(hostnames):
-        hostname = f"{hostname.strip()}"
-
-        try:
-            print(f"\n[{index}] ---> Resolving \"{hostname}\" <---\n")
-            ipv4 = socket.gethostbyname(hostname)
-            ipv4_list.append(f"{hostname} {ipv4}")
-            print(f"IPv4 : {ipv4}")
-
-        except Exception as err:
-            print(f"Error -> {err}")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for hostname in stripped_hostnames:
+            executor.submit(resolve_hostname, hostname)
 
     save_ipv4_list("./ipv4_list.txt")
--- Quick_and_dirty_scripts_enum/getIPv6.py
+++ Quick_and_dirty_scripts_enum/getIPv6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import socket
+import concurrent.futures
 
 ipv6_list = []
 
@@ -18,25 +19,29 @@ def save_ipv6_list(filename):
         for ipv6 in ipv6_list:
             ipv6_file.write(f"{ipv6}\n")
 
+    print(f"\n[+] Written file to {filename}")
+
+def resolve_hostname(hostname):
+    try:
+        result = socket.getaddrinfo(hostname, 443, socket.AF_INET6)
+        ipv6 = result[0][4][0]
+        ipv6_list.append(f"{hostname} {ipv6}")
+        print(f"{hostname} :: {ipv6}")
+
+    except Exception as err:
+        #print(f"{hostname} :: {err}")
+        pass
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Get IPv6 from hostname")
     parser.add_argument("file_to_read", help="Enter file containing a list of hostnames")
 
     args = parser.parse_args()
-    hostnames = read_from_file(args.file_to_read.strip())
+    hostnames = read_from_file(args.file_to_read)
+    stripped_hostnames = [hostname.strip() for hostname in hostnames]
 
-    # Check hostname 1 by 1
-    for index, hostname in enumerate(hostnames):
-        hostname = f"{hostname.strip()}"
-
-        try:
-            print(f"\n[{index}] ---> Resolving \"{hostname}\" <---\n")
-            result = socket.getaddrinfo(hostname, 443, socket.AF_INET6)
-            ipv6 = result[0][4][0]
-            ipv6_list.append(f"{hostname} {ipv6}")
-            print(f"IPv6 : {ipv6}")
-
-        except Exception as err:
-            print(f"Error -> {err}")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor: 
+        for hostname in stripped_hostnames:
+            executor.submit(resolve_hostname, hostname)
 
     save_ipv6_list("./ipv6_list.txt")
