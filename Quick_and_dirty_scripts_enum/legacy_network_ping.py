db13df8283418e5453e93cefff3f09be5ff03a6c
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-03-14 02:16:19 +0800

    Update legacy_network_ping.py

--- Quick_and_dirty_scripts_enum/legacy_network_ping.py
+++ Quick_and_dirty_scripts_enum/legacy_network_ping.py
@@ -7,12 +7,22 @@ from ping3 import ping
 
 responsive_hosts = []
 
+def banner():
+    intro = """
+    ██████  ██ ███    ██  ██████      ███████ ██     ██ ███████ ███████ ██████  ███████ ██████  
+    ██   ██ ██ ████   ██ ██           ██      ██     ██ ██      ██      ██   ██ ██      ██   ██ 
+    ██████  ██ ██ ██  ██ ██   ███     ███████ ██  █  ██ █████   █████   ██████  █████   ██████  
+    ██      ██ ██  ██ ██ ██    ██          ██ ██ ███ ██ ██      ██      ██      ██      ██   ██ 
+    ██      ██ ██   ████  ██████      ███████  ███ ███  ███████ ███████ ██      ███████ ██   ██ 
+    """
+    print(intro)
+
 def save_hosts_to_file(save_file):
     with open(save_file,'w') as sf:
         for host in responsive_hosts:
             sf.write(f"{host}\n")
 
-    print(f"\n[+] Saved file to {save_file}\n")
+    print(f"\n[+] Saved file to {save_file}")
 
 def ping_host(host):
     try:
@@ -41,25 +51,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     ipv4_subnet = args.ipv4_subnet
     ipv4_hosts = [str(ip).strip() for ip in ipaddress.IPv4Network(ipv4_subnet)]
-   
+  
+    banner()
     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
         for host in ipv4_hosts:
             executor.submit(ping_host, host)
 
-    '''
-    threads = list()
-    for host in ipv4_hosts:
-        t = threading.Thread(target=ping_host, args=(host,))
-        threads.append(t)
-        t.start()
-
-    for index, thread in enumerate(threads):
-        thread.join()
-    '''
-
-    print("\n[+] List of responsive hosts:\n")
+    print("\n[+] List of responsive hosts:")
     for host in responsive_hosts:
         print(host)
 
-    save_hosts_to_file("./ipv4_hosts.txt")
-
+    save_hosts_to_file(f"ipv4_hosts.txt")
