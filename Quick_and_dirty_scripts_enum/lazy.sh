b38a297233057503ceaaeb27ac7dba595a2ee951
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-04-21 01:37:31 +0800

    Update lazy.sh

--- Quick_and_dirty_scripts_enum/lazy.sh
+++ Quick_and_dirty_scripts_enum/lazy.sh
@@ -3,21 +3,27 @@ FNAME=$1
 
 if [ -z "$FNAME" ]
 then
-  echo "$0 file_containing_list_of_hostnames"
+  echo "Usage instructions:"
+  echo "$0 hostnames.txt"
+  echo -e "\nImportant:"
+  echo "Hostnames must contain http:// or https://"
+  exit 1
 
 else
   while read line; do
     # Remove https:// ; Only include domain names
     dname=$(echo $line | awk -F '//' '{print $2}')
 
-    echo "++ Performing top ports scan on $dname ++"
-    nmap -sC -sV -v $dname -oA "nmap-$dname"
+    echo -e "++ Performing top ports scan on $dname ++\n"
+    nmap -sT -sC -sV --min-rate 50 -v $dname -oA "nmap-$dname" 2> /dev/null
 
-    echo "++ Performing nikto scan on $dname ++"
-    nikto -h $dname -port 443 -Tuning x6 | tee "nikto-$dname"
+    echo -e "++ Performing nikto scan on $dname ++\n"
+    nikto -h $dname -port 443 -Tuning x6 2> /dev/null | tee "nikto-$dname"
 
-    echo "++ Performing dirb scan on $dname ++"
-    dirb $line -o "dirb-$dname"
+    echo -e "++ Performing dirb scan on $dname ++\n"
+    dirb $line -o "dirb-$dname" 2> /dev/null
   done < $FNAME
 
 fi
+
+exit 0
