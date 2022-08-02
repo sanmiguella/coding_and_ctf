3accfa83583045fbd9c7b49b9a50c831e9dd735a
Suhairy Subori <50333912+sanmiguella@users.noreply.github.com>
2022-08-02 16:56:35 +0800

    Update lazywhois.py

--- Quick_and_dirty_scripts_enum/lazywhois.py
+++ Quick_and_dirty_scripts_enum/lazywhois.py
@@ -16,8 +16,14 @@ def performWhois(ip):
     res = obj.lookup_whois()
     #pprint(res)
 
-    data  = f"Info: {res['nets'][0]['description']} | {res['query']} | {res['nets'][0]['cidr']}\r\n"
-    data += f"Asn: {res['asn']}\r\n"
+    if simpleView:
+        # Query | Cidr | Description | Asn
+        data = f"{res['query']} | {res['nets'][0]['cidr']} | {res['nets'][0]['description']} | {res['asn']}\r\n"
+    elif neatView:
+        data  = f"Query: {res['query']}\r\n"
+        data += f"Cidr: {res['nets'][0]['cidr']}\r\n"
+        data += f"Description: {res['nets'][0]['description']}\r\n"
+        data += f"Asn: {res['asn']}\r\n"
 
     print(data)
     return(data)
@@ -26,11 +32,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get whois data from IP address")
     parser.add_argument("file_to_read", help="File containing a list of IP addresses")
     parser.add_argument("-o", "--outfile", help="Output file to write results to", required=True)
+    parser.add_argument("-s", "--simple", help="Simple view", action='store_true')
+    parser.add_argument("-n", "--neat", help="Neat view", action='store_true')
 
     args = parser.parse_args()
     ipAddresses = readFromFile(args.file_to_read)
     ipList = [ipAddress.strip() for ipAddress in ipAddresses]
     outfile = args.outfile
+    simpleView = args.simple
+    neatView = args.neat
+
+    if simpleView and neatView:
+        print("\nEither -s or -n can be set, not both.\n")
+        sys.exit()
+    elif not simpleView and not neatView:
+        print("\nPlease set either -s or -n.\n")
+        sys.exit()
 
     with open(outfile, 'w') as f:
         for ip in ipList:
