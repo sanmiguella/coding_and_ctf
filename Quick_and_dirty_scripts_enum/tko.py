e6ab36db2dc84e76e85b684f09cf115e13a7e779
Suhairy Subori <50333912+sanmiguella@users.noreply.github.com>
2022-08-08 14:59:26 +0800

    testing

---
+++ Quick_and_dirty_scripts_enum/tko.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import dns.resolver
+import requests
+import concurrent.futures
+import argparse
+import sys
+
+def readFromFile(fileToRead):
+    with open(fileToRead) as f: return([line.strip() for line in f.readlines()])
+
+def checkForSubTakeover(host):
+    try:
+        answers = dns.resolver.query(host,'CNAME')
+        cname = ''
+        for rdata in answers: cname += str(rdata)
+        cnameResult = f'{answers.qname} > {cname}'
+    except:
+        cnameResult = f'{host} > No_cname_detected'
+    finally:
+        print(cnameResult)
+
+    try:
+        res = requests.get(f'https://{host}',verify=False).text
+
+        for fingerprint in fingerprintList:
+            if fingerprint in res:
+                msg = f'Takeover detected: {host} | {cnameResult} | Fingerprint: {fingerprint}'
+                takeover.append(msg)
+                print(msg)
+                break
+    except:
+        pass
+
+def saveToFile():
+    if len(takeover) > 0:
+        takeover.sort()
+        with open(outfile,'w') as f:
+            for result in takeover: f.write(f'{result}\n')
+
+        print(f'\n{len(takeover)} results saved...')
+    else:
+        print('\nNo results. Exiting program...')
+        sys.exit()
+
+if __name__ == "__main__":
+    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+    parser = argparse.ArgumentParser(description='Check list of hosts for subdomain takeover.')
+    parser.add_argument('hostsFileToRead', help='File containing list of hosts.')
+    parser.add_argument('-f', '--fingerprintFile', help='File containing fingerprints for subdomain takeover.', required=True)
+    parser.add_argument('-t', '--threads', help='Number of threads to use, default is 4.')
+    parser.add_argument('-o', '--outfile', help='File to write results to.')
+
+    takeover = []
+    args = parser.parse_args()
+    hostsList = readFromFile(args.hostsFileToRead)
+    fingerprintList = readFromFile(args.fingerprintFile)
+    outfile = args.outfile
+
+    tr = args.threads
+    if tr is None: tr = 4
+    else: tr = int(args.threads)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=tr) as executor:
+        print(f'Executing scan with {tr} threads.\n')
+        for host in hostsList: executor.submit(checkForSubTakeover,host)
+
+    saveToFile()
