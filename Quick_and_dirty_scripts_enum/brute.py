60c77c9909a084444d3a4c43e39f16243571f107
Suhairy Subori <50333912+sanmiguella@users.noreply.github.com>
2022-09-13 12:36:46 +0800

    Update brute.py
    Added functionality to bruteforce via dns

--- Quick_and_dirty_scripts_enum/brute.py
+++ Quick_and_dirty_scripts_enum/brute.py
@@ -3,6 +3,8 @@ import requests
 import argparse
 import sys
 import concurrent.futures
+import dns.resolver
+
 from urllib.parse import urlparse
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
@@ -20,9 +22,11 @@ class Scanner:
         try:
             with open(self.wordlist, 'r') as f:
                 data = [line.strip() for line in f.readlines()]
+
         except Exception as err:
             print(f"\n[!] {err}")
             sys.exit()
+
         else:
             return(data)
 
@@ -31,10 +35,13 @@ class Scanner:
             with open(self.outfile, 'w') as of:
                 for found in self.found:
                     of.write(f"{found}\n")
+
         except Exception as err:
             print(f"\n[!] {err}")
+
         else:
             print(f"\n[>] Wrote results to {self.outfile}\n")
+
         finally:
             self.found.clear()
 
@@ -42,10 +49,13 @@ class Scanner:
         try:
             url = f"{self.domainName}/{fuzz}"
             r = requests.get(url, timeout=self.timeout, verify=False)
+
         except requests.ConnectionError:
             pass
+
         except Exception as err:
             print(f"[!] {err}")
+
         else:
             responseCode = r.status_code
 
@@ -83,10 +93,13 @@ class SubDomainScanner(Scanner):
         try:
             subDomain = f"https://{fuzz}.{self.domainName}"
             requests.get(subDomain, verify=False)
+
         except requests.ConnectionError:
             pass
+
         except Exception as err:
             print(f"[!] {err}")
+
         else:
             print(f"Valid: {subDomain}")
             self.found.append(f"{fuzz}.{self.domainName}")
@@ -101,9 +114,11 @@ class MassDirBruteScan(Scanner):
         try:
             with open(fileToRead, 'r') as f:
                 data = [line.strip() for line in f.readlines()]
+
         except Exception as err:
             print(f"\n[!] {err}")
             sys.exit()
+
         else:
             return(data)
 
@@ -127,6 +142,50 @@ class MassDirBruteScan(Scanner):
 
             self.writeFile()
 
+class DnsScan(SubDomainScanner):
+    def __init__(self, domainName, wordlist, threadsNum, outfile):
+        super().__init__(domainName, wordlist, threadsNum, outfile)
+        self.recordType_A = 'A'
+        self.recordType_AAAA = 'AAAA'
+
+    def checkResponse(self, fuzz):
+        resolver = dns.resolver.Resolver() 
+        subDomain = f"{fuzz}.{self.domainName}"
+
+        resolvedToIPv4 = None
+        resolvedToIPv6 = None
+
+        try:
+            resolvedToIPv4 = resolver.query(subDomain, self.recordType_A)[0]
+
+        except:
+            pass
+
+        try:
+            resolvedToIPv6 = resolver.query(subDomain, self.recordType_AAAA)[0]
+
+        except:
+            pass
+
+        if (resolvedToIPv4 is not None) or (resolvedToIPv6 is not None):
+            print(f"Valid: {subDomain}")
+            self.found.append(subDomain)
+
+    def scan(self):
+        wordlist = self.readFile()
+
+        print()
+        print(f"[+] Performing DNS enumeration with {self.threadsNum} threads...")
+        print(f"[+] Wordlists contains {len(wordlist)} entries.")
+        print(f"[+] Output file - {self.outfile}\n")
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.threadsNum) as executor:
+            for fuzz in wordlist:
+                executor.submit(self.checkResponse, fuzz)
+
+        if self.outfile is not None:
+            self.writeFile()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bruteforce file|dir|subdomain(s).")
     subParser = parser.add_subparsers(dest="command")
@@ -142,10 +201,16 @@ if __name__ == "__main__":
     singleOrMassScanParser.add_argument("-d", "--domain", help="Domain format: https://test.com")
 
     subScanParser = subParser.add_parser("sub", help="Subdomain bruteforcing.")
-    subScanParser.add_argument("-d", "--domain", help="Domain format: test.com")
+    subScanParser.add_argument("-d", "--domain", help="Domain format: test.com", required=True)
     subScanParser.add_argument("-w", "--wordlist", help="Wordlist.", required=True)
     subScanParser.add_argument("-o", "--outfile", help="Writes results to file.")
     subScanParser.add_argument("-t", "--threads", nargs="?", const=10, type=int, default=10, help="Threads. Default is 10.")
+
+    dnsScanParser = subParser.add_parser("dns", help="Dns bruteforcing.")
+    dnsScanParser.add_argument("-d", "--domain", help="Domain forma: test.com", required=True)
+    dnsScanParser.add_argument("-w", "--wordlist", help="Wordlist.", required=True)
+    dnsScanParser.add_argument("-o", "--outfile", help="Writes results to file.")
+    dnsScanParser.add_argument("-t", "--threads", nargs="?", const=10, type=int, default=10, help="Threads. Default is 10.")
     
     args = parser.parse_args()
     command = args.command
@@ -175,6 +240,10 @@ if __name__ == "__main__":
     elif command == "sub":
         subBrute = SubDomainScanner(args.domain, args.wordlist, args.threads, args.outfile)
         subBrute.scan()
+
+    elif command == "dns":
+       dnsBrute = DnsScan(args.domain, args.wordlist, args.threads, args.outfile) 
+       dnsBrute.scan()
 
     else:
         parser.print_usage()
