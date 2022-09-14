794866e9f1877c76dd9aef03d85aeba2db5c7924
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-09-15 02:10:41 +0800

    Update scanhttp.py

--- Quick_and_dirty_scripts_enum/scanhttp.py
+++ Quick_and_dirty_scripts_enum/scanhttp.py
@@ -80,7 +80,7 @@ if __name__=="__main__":
     parser.add_argument("-f", "--file", help="Host file.", required=True)
     parser.add_argument("-vf", "--validOutputFile", nargs="?", const="vf.txt", default="vf.txt", help="Writes to file a list of HTTP 200 ok hosts. Default file is vf.txt.")
     parser.add_argument("-of", "--otherOutputFile", nargs="?", const="of.txt", default="of.txt", help="Writes to file a list of hosts where response code isn't HTTP 200. Default file is of.txt.")
-    parser.add_argument("-t", "--threads", nargs="?", const=10, type=int, default=10,  help="Threads. Default is 10.")
+    parser.add_argument("-t", "--threads", nargs="?", const=20, type=int, default=20,  help="Threads. Default is 20.")
     args = parser.parse_args()
 
     scanner = HttpScan(args.file, args.validOutputFile, args.otherOutputFile, args.threads)
