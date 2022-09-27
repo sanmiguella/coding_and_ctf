a48c12a141d1614a4dd8d6a9f27d1b7ca5929060
Suhairy Subori <50333912+sanmiguella@users.noreply.github.com>
2022-09-27 14:47:10 +0800

    Update totaler.py

--- Quick_and_dirty_scripts_enum/totaler.py
+++ Quick_and_dirty_scripts_enum/totaler.py
@@ -3,14 +3,19 @@ from xml.dom import minidom
 import argparse
 import texttable
 
-def parseXML(fname):
-    global totalHostCount
-    global totalPortCount
-
+def parseXML():
     # Reference: https://mkyong.com/python/python-read-xml-file-dom-example/
     doc = minidom.parse(fname)
     hostTag = doc.getElementsByTagName('host')
 
+    tableObj = texttable.Texttable(0)
+    tableObj.set_cols_align(["l", "l", "l", "l"])
+    tableObj.set_cols_dtype(["t", "t", "t", "t"])
+    #tableObj.set_deco(tableObj.HEADER) # Comment out to include borders.
+    tableObj.add_row(['IP Address', 'Hostname', 'Num of Port(s)', 'Port(s) open'])
+
+    totalHostCount = 0
+    totalPortCount = 0
     portsFound = False
 
     for ht in hostTag:
@@ -30,8 +35,9 @@ def parseXML(fname):
 
         portsPerHostCount = 0
         try:
+            portID = ''
             for p in portsTag:
-                #portID = p.getAttribute('portid')
+                portID += p.getAttribute('portid') + ' '
                 portsPerHostCount += 1
                 totalPortCount += 1
                 portsFound = True
@@ -45,24 +51,15 @@ def parseXML(fname):
 
         # Only add entries if ports per host > 0
         if portsPerHostCount > 0:
-            tableObj.add_row([ipAddr, hostname, portsPerHostCount])
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Get total number of hosts and services from Nmap xml file(s).")
-    parser.add_argument("-f", "--xmlFile", type=argparse.FileType('r'), help="One or more Nmap xml file(s) to read data from.", nargs='+', required=True)
-    args = parser.parse_args()
-
-    fnames = args.xmlFile
-
-    tableObj = texttable.Texttable(0)
-    tableObj.set_cols_align(["l", "l", "l"])
-    tableObj.set_cols_dtype(["t", "t", "t"])
-    tableObj.set_deco(tableObj.HEADER) # Comment out to include borders.
-    tableObj.add_row(['IP Address', 'Hostname', 'Num of Port(s)'])
-
-    totalHostCount = 0
-    totalPortCount = 0
-    for f in fnames: parseXML(f)
+            tableObj.add_row([ipAddr, hostname, portsPerHostCount, portID])
 
     print(tableObj.draw()) 
     print(f'\nTotal host(s): {totalHostCount}\nTotal port(s): {totalPortCount}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get total number of hosts and services from nmap xml file.")
+    parser.add_argument("-f", "--xmlFile", help="Nmap xml file to read data from.", required=True)
+    args = parser.parse_args()
+
+    fname = args.xmlFile
+    parseXML()
