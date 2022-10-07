401483091bf09f308b66773d918b4f1d863a1bdf
Suhairy Subori <50333912+sanmiguella@users.noreply.github.com>
2022-10-07 13:11:53 +0800

    Update checkLinks.py

--- Quick_and_dirty_scripts_enum/checkLinks.py
+++ Quick_and_dirty_scripts_enum/checkLinks.py
@@ -1,60 +1,65 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 import argparse
 import re
+import numpy as np
+import concurrent.futures
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
-links = []
-
-def banner():
-    intro = '''
-    ██╗     ██╗███╗   ██╗██╗  ██╗███████╗
-    ██║     ██║████╗  ██║██║ ██╔╝██╔════╝
-    ██║     ██║██╔██╗ ██║█████╔╝ ███████╗
-    ██║     ██║██║╚██╗██║██╔═██╗ ╚════██║
-    ███████╗██║██║ ╚████║██║  ██╗███████║
-    ╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
-    '''
-    print(intro)
-
-def save_links(filename):
+def save_links(filename, links):
     with open(filename,'w') as f:
-        for site in links:
-            f.write(f"{site}\n")
+        for link in links:
+            f.write(f"{link}\n")
 
     print(f"\n[+] Saved file to {filename}")
 
 def read_from_file(file_to_read):
     with open(file_to_read,'r') as f:
-        hostnames = f.readlines()
+        urls = [url.strip() for url in f.readlines()]
 
-    return(hostnames)
+    return(urls)
 
-def check_for_links(url):
+def check_webpage_for_links(url):
     try:
-        html_content = requests.get(url).text
+        html_content = requests.get(url, verify=False).text
         soup = BeautifulSoup(html_content, "lxml")
 
         for link in soup.find_all('a', attrs={'href': re.compile("^https://")}):
-            link_only = link['href']
+            link_only = urljoin(url, link['href'])
             links.append(link_only)
             print(link_only) 
 
     except Exception as err:
-        print(f"[!] Error :: {err}")
+        print(f"[!] {err}")
+
+def read_from_file(file_to_read):
+    with open(file_to_read, 'r') as f:
+        urls = [url.strip() for url in f.readlines()]
+
+    return urls
 
 if __name__ == "__main__":
+    links = list()
+
     parser = argparse.ArgumentParser(description="Get Links from Url")
-    parser.add_argument("-u", "--url", help="URL containing Links.", required=True)
+    parser.add_argument("-f", "--file", help="File containing list of hosts that starts with https://", required=True)
     parser.add_argument("-o", "--outfile", help="File to write all links to.")
+    parser.add_argument("-t", "--threads", nargs="?", const=20, type=int, default=20, help="Number of threads, default is 20.")
 
     args = parser.parse_args()
     outfile = args.outfile
-    url = args.url
-    url = url.strip()
+    urls = read_from_file(args.file)
 
-    banner()
-    check_for_links(url)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+        print(f"[+] Scanning with {args.threads} threads.\n")
+
+        for url in urls:
+            executor.submit(check_webpage_for_links, url)
+
+    links = np.unique(links).tolist()
+    links.sort()
 
     if outfile:
-        save_links(outfile)
+        save_links(outfile, links)
