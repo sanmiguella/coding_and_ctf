5fc2cdedc703e72ecad2c4ecead91f75f27fb73a
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-10-22 21:56:06 +0800

    Update tko.py

--- Quick_and_dirty_scripts_enum/tko.py
+++ Quick_and_dirty_scripts_enum/tko.py
@@ -5,14 +5,20 @@ import concurrent.futures
 import argparse
 import sys
 
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
 def readFromFile(fileToRead):
-    with open(fileToRead) as f: return([line.strip() for line in f.readlines()])
+    with open(fileToRead) as f: 
+        return [line.strip() for line in f.readlines()]
 
 def checkForSubTakeover(host):
     try:
-        answers = dns.resolver.resolve(host,'CNAME')
+        answers = dns.resolver.resolve(host, 'CNAME')
         cname = ''
-        for rdata in answers: cname += str(rdata)
+
+        for rdata in answers: 
+            cname += str(rdata)
+
         cnameResult = f'{answers.qname} - {cname} (CNAME)'
     except:
         cnameResult = f'{host} - NoCNAME'
@@ -23,13 +29,13 @@ def checkForSubTakeover(host):
         
         for fingerprint in fingerprintList:
             if fingerprint in res:
-                msg = f'Host: {host} | {cnameResult} | Fingerprint: "{fingerprint}"'
+                msg = f'{host} | {cnameResult} | Fingerprint: "{fingerprint}"'
                 takeover.append(msg)
                 notFound = False
                 break
         
         if notFound:
-            msg = f'Host: {host} | {cnameResult} | No fingerprint matched'
+            msg = f'{host} | {cnameResult} | No fingerprint matched'
     except Exception as err:
         msg = f'Host: {host} | Error: {err}'
     finally:
@@ -40,45 +46,32 @@ def saveToFile():
         takeover.sort()
 
         with open(outfile,'w') as f:
-            for result in takeover: f.write(f'{result}\n')
+            for result in takeover: 
+                f.write(f'{result}\n')
 
         print(f'\n{len(takeover)} results saved...')
     else:
         print('\nNo results. Exiting program...')
         sys.exit()
 
-def showBanner():
-    banner = '''
-    ███████ ██    ██ ██████  ████████ ██   ██  ██████  
-    ██      ██    ██ ██   ██    ██    ██  ██  ██    ██ 
-    ███████ ██    ██ ██████     ██    █████   ██    ██ 
-         ██ ██    ██ ██   ██    ██    ██  ██  ██    ██ 
-    ███████  ██████  ██████     ██    ██   ██  ██████  
-    '''
-    print(banner)
-
 if __name__ == "__main__":
-    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-
-    showBanner()
     parser = argparse.ArgumentParser(description='Check list of hosts for subdomain takeover.')
     parser.add_argument('hostsFileToRead', help='File containing list of hosts.')
-    parser.add_argument('-f', '--fingerprintFile', help='File containing fingerprints for subdomain takeover.',required=True)
-    parser.add_argument('-t', '--threads', help='Number of threads to use, default is 10.')
-    parser.add_argument('-o', '--outfile', help='File to write results to.',required=True)
+    parser.add_argument('-f', '--fingerprintFile', help='File containing fingerprints for subdomain takeover.', required=True)
+    parser.add_argument('-t', '--threads', nargs='?', const=20, type=int, default=20,  help='Number of threads to use, default is 20.')
+    parser.add_argument('-o', '--outfile', nargs='?', const='tko-results.txt', default='tko-results.txt',  help='File to write results to.')
 
-    takeover = []
+    takeover = list()
     args = parser.parse_args()
     hostsList = readFromFile(args.hostsFileToRead)
     fingerprintList = readFromFile(args.fingerprintFile)
     outfile = args.outfile
 
     tr = args.threads
-    if tr is None: tr = 10
-    else: tr = int(args.threads)
 
+    print(f'Executing scan with {tr} threads.\n')
     with concurrent.futures.ThreadPoolExecutor(max_workers=tr) as executor:
-        print(f'Executing scan with {tr} threads.\n')
-        for host in hostsList: executor.submit(checkForSubTakeover,host)
+        for host in hostsList: 
+            executor.submit(checkForSubTakeover, host)
 
     saveToFile()
