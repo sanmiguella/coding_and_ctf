b2a553c594a5fdc3921c9142c74e18d5c2ab8498
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-11-17 23:01:34 +0800

    Create portSearcher.py

---
+++ Quick_and_dirty_scripts_enum/portSearcher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from xml.dom import minidom
+import argparse
+
+def parse_xml():
+    xmlDoc = minidom.parse(xmlFile)
+    hostTag = xmlDoc.getElementsByTagName('host')
+
+    for host in hostTag:
+        addressTag = host.getElementsByTagName('address')[0]
+        ipAddr = addressTag.getAttribute('addr')
+
+        host_portList = list()
+        portsTag = host.getElementsByTagName('port')
+
+        for singlePort in portsTag:
+            portNum = int(singlePort.getAttribute('portid'))
+            host_portList.append(portNum)
+
+        if len(host_portList) > 0:
+            # https://thispointer.com/python-check-if-a-list-contains-all-the-elements-of-another-list/
+            listedPortsExistOnHost = all(port in host_portList for port in portList)
+            
+            if listedPortsExistOnHost:
+                ports =  ' '.join(str(port) for port in host_portList)
+                print(f'[+] {portList} found on {ipAddr} - {ports}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Get host from port(s)')
+    parser.add_argument('-f', '--xmlfile', help='Nmap xml file to read data from', required=True)
+    parser.add_argument('-p', '--ports', nargs='+', type=int,  help='Ports separated by space', required=True)
+
+    args = parser.parse_args()
+    xmlFile = args.xmlfile
+    portList = args.ports
+
+    parse_xml()
