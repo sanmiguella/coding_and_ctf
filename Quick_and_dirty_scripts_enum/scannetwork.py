12b1fd60c3bf54477254963424d46ba18741662a
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-12-11 23:15:10 +0800

    Update scannetwork.py

--- Quick_and_dirty_scripts_enum/scannetwork.py
+++ Quick_and_dirty_scripts_enum/scannetwork.py
@@ -35,22 +35,25 @@ def scanPort(ip, port):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TCP port scanner.')
-    parser.add_argument('-s', '--subnet', help='Subnet to scan.')
-    parser.add_argument('-d', '--dir', help='Directory to save results to.')
-    parser.add_argument('-t', '--threads', help='Number of threads.')
+    parser.add_argument('-d', '--dir', help='Directory to save results to.', required=True)
+    parser.add_argument('-t', '--threads', help='Number of threads.', required=True)
+
+    parseOpt = parser.add_mutually_exclusive_group(required=True)
+    parseOpt.add_argument('-s', '--subnet', help='Subnet to scan.')
+    parseOpt.add_argument('-f', '--filelist', help='File containing list of host(s)')
     
     args = parser.parse_args()
-    subnet = args.subnet
     saveDir = args.dir
     numOfThreads = args.threads
 
-    # https://stackoverflow.com/questions/1942160/python-3-create-a-list-of-possible-ip-addresses-from-a-cidr-notation
-    ipList = ipaddress.ip_network(subnet)
-
     datetimeFormat = "%d/%m/%Y %H:%M:%S"
 
-    if subnet and saveDir and numOfThreads:
-        try:
+    try:
+        if args.subnet:
+            # https://stackoverflow.com/questions/1942160/python-3-create-a-list-of-possible-ip-addresses-from-a-cidr-notation
+            subnet = args.subnet
+            ipList = ipaddress.ip_network(subnet)
+
             for ip in ipList.hosts():
                 openPorts = list()
 
@@ -71,10 +74,34 @@ if __name__ == '__main__':
 
                 openPorts.clear()
 
-        except KeyboardInterrupt:
-            print('\n[!] Exiting now.')
+        elif args.filelist:
+            fileList = args.filelist
 
-        except Exception as err:
-            print(f'\n[!] {err}')
-    else:
-        parser.print_help()
+            with open(fileList, 'r') as f:
+                hostList = list(host.strip() for host in f.readlines())
+
+            for ip in hostList:
+                openPorts = list()
+
+                print(f'\nTarget :: {ip}')
+                print(f'Started scan at :: {str(datetime.now().strftime(datetimeFormat))}\n')
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=int(numOfThreads)) as executor:
+                    for port in range(1, 65535):
+                        executor.submit(scanPort, ip, port)
+
+                print(f'\nScan completed for {ip} at :: {str(datetime.now().strftime(datetimeFormat))}')
+
+                if saveDir:
+                    os.makedirs(saveDir, exist_ok=True)
+                    saveOpenPorts(ip, saveDir)
+                else:
+                    saveOpenPorts(ip, '')
+
+                openPorts.clear()
+
+    except KeyboardInterrupt:
+        print('\n[!] Exiting now.')
+
+    except Exception as err:
+        print(f'\n[!] {err}')
