a6f8739968754b2802765d726dd31084b07e2ccc
evdaez <50333912+sanmiguella@users.noreply.github.com>
2022-12-12 21:47:34 +0800

    Update scannetwork.py

--- Quick_and_dirty_scripts_enum/scannetwork.py
+++ Quick_and_dirty_scripts_enum/scannetwork.py
@@ -4,6 +4,7 @@ import argparse
 import socket
 import concurrent.futures
 import os
+import sys
 from datetime import datetime
 
 def saveOpenPorts(ip, saveDir):
@@ -12,19 +13,17 @@ def saveOpenPorts(ip, saveDir):
     else:
         filename = f'portscan-{ip}.txt'
 
-    openPorts.sort()
-
     with open(filename, 'w') as f:
-        f.write(f'{ip}\n\n')
+        f.write(f'host : {ip}\n')
 
         for port in openPorts:
-            f.write(f'{port} / tcp\n')
+            f.write(f'{port} : open\n')
 
     print(f'Saved results to :: {filename}')
 
 def scanPort(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
+    #socket.setdefaulttimeout(1)
     result = sock.connect_ex((ip, port))
 
     if result == 0:
@@ -33,75 +32,86 @@ def scanPort(ip, port):
 
     sock.close()
 
+def startScan(ipList):
+    for ip in ipList:
+        print(f'\nTarget :: {ip}')
+        print(f'Started scan at :: {str(datetime.now().strftime(datetimeFormat))}\n')
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=numOfThreads) as executor:
+            for port in range(1, 65535):
+                executor.submit(scanPort, ip, port)
+
+        print(f'\nScan completed for {ip} at :: {str(datetime.now().strftime(datetimeFormat))}')
+        openPorts.sort()
+
+        if saveDir:
+            os.makedirs(saveDir, exist_ok=True)
+            saveOpenPorts(ip, saveDir)
+        else:
+            saveOpenPorts(ip, '')
+
+        openPorts.clear()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TCP port scanner.')
     parser.add_argument('-d', '--dir', help='Directory to save results to.', required=True)
-    parser.add_argument('-t', '--threads', help='Number of threads.', required=True)
+    parser.add_argument('-t', '--threads', nargs='?', const=50, type=int, default=50, help='Number of threads. Default is 50.')
 
     parseOpt = parser.add_mutually_exclusive_group(required=True)
     parseOpt.add_argument('-s', '--subnet', help='Subnet to scan.')
     parseOpt.add_argument('-f', '--filelist', help='File containing list of host(s)')
+    parseOpt.add_argument('-i', '--ip', nargs='+', help='Target IP(s) separated by spaces.')
     
     args = parser.parse_args()
     saveDir = args.dir
     numOfThreads = args.threads
 
-    datetimeFormat = "%d/%m/%Y %H:%M:%S"
+    datetimeFormat = '%d/%m/%Y %H:%M:%S'
+    openPorts = list()
 
     try:
         if args.subnet:
             # https://stackoverflow.com/questions/1942160/python-3-create-a-list-of-possible-ip-addresses-from-a-cidr-notation
-            subnet = args.subnet
-            ipList = ipaddress.ip_network(subnet)
+            try:
+                hostList = ipaddress.ip_network(args.subnet)
+                ipList = list(str(ip) for ip in hostList.hosts())
 
-            for ip in ipList.hosts():
-                openPorts = list()
+            except Exception as err:
+                print(f'[!] {err}')
 
-                print(f'\nTarget :: {ip}')
-                print(f'Started scan at :: {str(datetime.now().strftime(datetimeFormat))}\n')
-
-                with concurrent.futures.ThreadPoolExecutor(max_workers=int(numOfThreads)) as executor:
-                    for port in range(1, 65535):
-                        executor.submit(scanPort, str(ip), port)
-
-                print(f'\nScan completed for {ip} at :: {str(datetime.now().strftime(datetimeFormat))}')
-
-                if saveDir:
-                    os.makedirs(saveDir, exist_ok=True)
-                    saveOpenPorts(ip, saveDir)
-                else:
-                    saveOpenPorts(ip, '')
-
-                openPorts.clear()
+            else:
+                startScan(ipList)
 
         elif args.filelist:
-            fileList = args.filelist
+            try:
+                with open(args.filelist, 'r') as f:
+                    ipList = list(host.strip() for host in f.readlines())
 
-            with open(fileList, 'r') as f:
-                hostList = list(host.strip() for host in f.readlines())
+            except Exception as err:
+                print(f'[!] {err}')
 
-            for ip in hostList:
-                openPorts = list()
+            else:
+                try:
+                    [ipaddress.ip_address(ip) for ip in ipList]
+                
+                except Exception as err:
+                    print(f'[!] {err}')
 
-                print(f'\nTarget :: {ip}')
-                print(f'Started scan at :: {str(datetime.now().strftime(datetimeFormat))}\n')
-
-                with concurrent.futures.ThreadPoolExecutor(max_workers=int(numOfThreads)) as executor:
-                    for port in range(1, 65535):
-                        executor.submit(scanPort, ip, port)
-
-                print(f'\nScan completed for {ip} at :: {str(datetime.now().strftime(datetimeFormat))}')
-
-                if saveDir:
-                    os.makedirs(saveDir, exist_ok=True)
-                    saveOpenPorts(ip, saveDir)
                 else:
-                    saveOpenPorts(ip, '')
+                    startScan(ipList)
 
-                openPorts.clear()
+        elif args.ip:
+            ipList = args.ip
+
+            try:
+                [ipaddress.ip_address(ip) for ip in ipList]
+
+            except Exception as err:
+                print(f'[!] {err}')
+
+            else:
+                startScan(ipList)
 
     except KeyboardInterrupt:
         print('\n[!] Exiting now.')
-
-    except Exception as err:
-        print(f'\n[!] {err}')
+        sys.exit(1)
